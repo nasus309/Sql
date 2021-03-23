290a1a14f04243b772c8339359b4313582b843b2
nasus309 <skgus9622@naver.com>
2021-03-23 15:13:09 +0900

    2021/03/23 SQL 기본 실습예제 추가

---
+++ 1_SQL기본.sql
@@ -0,0 +1,14 @@
+#날짜 : 2021/03/23
+#이름 : 김나현
+#내용 : SQL 기본 실습하기
+
+#실습하기 2-1
+CREATE DATABASE `MyDB`;
+DROP DATABASE `MyDB`;
+
+CREATE TABLE `USER1` (
+	`uid` VARCHAR(10),
+	`name` VARCHAR(10),
+	`hp`	CHAR(10),
+	`age` INT
+);
